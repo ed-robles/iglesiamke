2b44e1cec7bae34fab5dc3ec34f7163b2b0e1c17
Edgar Robles <robles_edgar@icloud.com>
2025-01-08 16:13:28 -0600

    Add HeroSection component and update Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import Header from "@/components/Header";
+import HeroSection from "@/components/HeroSection";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
+    <div>
       <Header />
-      <div className="">
-        <h1 className="p-2 font-bold text-lg">BIENVENIDOS</h1>
+      <HeroSection />
+
+      <div className="m-2">
+        <p className="text-2xl font-bold">
+          Bienvenidos a Primera Iglesia Bautista de Milwaukee
+        </p>
       </div>
     </div>
   );
---
+++ components/HeroSection.tsx
@@ -0,0 +1,9 @@
+const HeroSection = () => {
+  return (
+    <div className="w-1/3 mx-auto mt-2">
+      <img src={"/coverImage-edit.jpg"} alt="cover" />
+    </div>
+  );
+};
+
+export default HeroSection;
