12abe43b52d60cd0c2ae9df76322f7df0f874540
Edgar Robles <robles_edgar@icloud.com>
2025-01-09 22:31:06 -0600

    Enhance Home and Banner components with improved text styling and layout adjustments for better readability and responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,15 @@ export default function Home() {
     <div>
       <Header />
       <HeroSection />
-      <div className="flex flex-col items-center justify-center py-16 px-4 lg:px-8">
-        <h1 className="text-2xl lg:text-4xl text-center bg-[#00416a] text-white p-4 rounded-lg shadow">
-          Bienvenidos A Nuestra Iglesia
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="md:hidden text-2xl lg:text-4xl font-bold text-center bg-[#00416a] w-full text-white py-8">
+          Bienvenidos a<br /> Nuestra Iglesia
         </h1>
-        <div className="text-lg lg:text-xl text-center text-black mt-4">
-          <h2 className="font-bold underline my-8 text-2xl">Horario</h2>
+        <h1 className="hidden md:block text-2xl lg:text-4xl font-bold text-center bg-[#00416a] w-full text-white py-8">
+          Bienvenidos a Nuestra Iglesia
+        </h1>
+        <div className="text-lg lg:text-xl text-center text-white bg-gray-800 m-8 p-4 rounded shadow-lg max-w-2xl mx-auto">
+          <h2 className="font-bold underline text-2xl mb-4">Horario</h2>
           <p className="mb-2">Domingo: 11:00 AM - 12:00 PM</p>
           <p className="mb-2">Miercoles: 6:30 PM - 8:00 PM</p>
           <p className="mb-2">Sabado: 10:00 AM - 12:00 PM</p>
