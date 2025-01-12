69373f22a2b2e75d1b020bc2270975a9d48ec9f2
Edgar Robles <robles_edgar@icloud.com>
2025-01-11 19:32:34 -0600

    Update schedule details in Home component for improved clarity and accuracy

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,12 @@ export default function Home() {
 
       <div className="text-lg lg:text-xl text-center text-white bg-gray-800 p-8 w-full mx-auto">
         <h2 className="font-bold underline text-2xl mb-4">Horario</h2>
-        <p className="mb-2">Domingo: 11:00 AM - 12:00 PM</p>
-        <p className="mb-2">Miercoles: 6:30 PM - 8:00 PM</p>
-        <p className="mb-2">Sabado: 10:00 AM - 12:00 PM</p>
+        <p className="mb-2">Domingo - Escuela Dominical - 10:00am</p>
+        <p className="mb-2">Domingo - Servicio - 11:00am</p>
+        <p className="mb-2">Domingo - Servicio - 5:00pm</p>
+        <p className="mb-2">Miercoles - Estudio Biblico - 7:00pm</p>
       </div>
+
       <Banner />
     </div>
   );
