485f69128ccd54bb73ab79588df1f00a21ca2634
Edgar Robles <robles_edgar@icloud.com>
2025-01-11 19:40:44 -0600

    Refactor schedule section in Home component for improved structure and readability

--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,17 @@ export default function Home() {
       <HeroSection />
 
       <div className="text-lg lg:text-xl text-center text-white bg-gray-800 p-8 w-full mx-auto">
-        <h2 className="font-bold underline text-2xl mb-4">Horario</h2>
-        <p className="mb-2">Domingo - Escuela Dominical - 10:00am</p>
-        <p className="mb-2">Domingo - Servicio - 11:00am</p>
-        <p className="mb-2">Domingo - Servicio - 5:00pm</p>
-        <p className="mb-2">Miercoles - Estudio Biblico - 7:00pm</p>
+        <h1 className="font-bold underline text-2xl mb-4">Horario</h1>
+        <h2 className="mb-2">
+          <span className="font-bold">Domingo</span>
+          <p>Escuela Dominical: 10:00am</p>
+          <p>Servicio: 11:00am</p>
+          <p>Servicio: 5:00pm</p>
+        </h2>
+        <h2 className="mt-4">
+          <span className="font-bold">Miercoles</span>
+          <p>Estudio Biblico: 7:00pm</p>
+        </h2>
       </div>
 
       <Banner />
